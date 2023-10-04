a1b4f5484035466c9215fead5d24b82bb9737f14
BachVanHoach <117200521+BachVanHoach@users.noreply.github.com>
2023-10-04 18:41:44 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ app_dir = os.path.abspath(os.path.dirname(__file__))
 
 class BaseConfig:
     DEBUG = True
-    POSTGRES_URL="lab3-adop-1.postgres.database.azure.com"
+    POSTGRES_URL="lab3-adop-2.postgres.database.azure.com"
     POSTGRES_USER="adminlab3"
     POSTGRES_PW="congtu123!"
     POSTGRES_DB="postgres"
@@ -12,7 +12,7 @@ class BaseConfig:
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
     CONFERENCE_ID = 1
     SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
-    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://hoachbv-lab3-svb.servicebus.windows.net/;SharedAccessKeyName=SAS;SharedAccessKey=2lA9Sky8uSFn9K1O5u3rILc/zaP9UIAVy+ASbG8oIYI=;EntityPath=notificationqueue' #TODO: Update value
+    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://hoachbv-lab3-svb.servicebus.windows.net/;SharedAccessKeyName=policies;SharedAccessKey=w6HZmcnlKdjjfxocuwRJfjchaosLyPxFv+ASbNNGupM=;EntityPath=notificationqueue'
     SERVICE_BUS_QUEUE_NAME ='notificationqueue'
     ADMIN_EMAIL_ADDRESS: 'info@techconf.com'
     SENDGRID_API_KEY = '' #Configuration not required, required SendGrid Account
